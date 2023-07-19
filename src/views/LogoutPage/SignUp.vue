321a3c7f078d4ea6e26192f2b4b37c61f355e3f0
Dery Jon <yusupoviskandar1005@gmail.com>
2023-07-19 18:46:10 +0500

    fiz: delete logout (2023-07-19, 18:46)

--- src/views/LogoutPage/SignUp.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container>
-    <SignUp />
-  </v-container>
-</template>
-<script>
-import SignUp from "../../components/SignUp.vue";
-export default { components: { SignUp } };
-</script>
