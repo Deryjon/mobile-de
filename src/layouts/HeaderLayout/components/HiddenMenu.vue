07ce58ddbc35a6eda3abf1e47da9509ee33bbe1e
Dery Jon <yusupoviskandar1005@gmail.com>
2023-07-29 14:51:17 +0500

    fixed: fixed bugs (2023-07-29, 14:51)

--- src/layouts/HeaderLayout/components/HiddenMenu.vue
+++ src/layouts/HeaderLayout/components/HiddenMenu.vue
@@ -1,12 +1,9 @@
-<template >
+<template>
   <div
     class="hidden-menu fixed right-0 top-0 w-1.5/2 h-full p-5 flex flex-col "
-	
     :class="{ 'bg-[#526d82]': isDarkMode, 'bg-[#F8FCFF]': !isDarkMode }"
   >
-    <div class="wrapper"
-
-		>
+    <div class="wrapper">
       <div class="head flex items-center justify-between pt-3">
         <HeaderLinks />
 
@@ -19,23 +16,23 @@
         <HeaderLogo @click="$emit('closeMenu')" class="order-2" />
       </div>
 
-      <div class="body ">
+      <div class="body">
         <HeaderSwitcherTheme
-          class="ml-[28px] mt-[10px]"
+          class="ml-[30px] mt-[10px]"
           @click="$emit('closeMenu')"
         />
-        <HeaderActions class="flex flex-col mr-[38px]" />
-        <HeaderContact class="flex justify-center ml-[30px] mt-[10px]" 
-				@click="$emit('closeMenu')"
-				/>
-				<HeaderLogout class="flex justify-center ml-[30px] mt-[10px]"
-				
-				/>
+        <HeaderActions class="flex flex-col justify-between mr-[30px]" />
+        <HeaderContact
+          class="ml-[35px] mt-[10px]"
+          @click="$emit('closeMenu')"
+        />
+        <HeaderLogout
+          class="flex justify-center ml-[35px] mt-[10px]"
+        />
       </div>
     </div>
   </div>
-	
-</template >
+</template>
 
 <script>
 import HeaderLogo from "./HeaderLogo.vue";
