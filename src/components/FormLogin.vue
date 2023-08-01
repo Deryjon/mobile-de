b93041aeda4df945e631be55b5205f6c359aae7c
Dery Jon <yusupoviskandar1005@gmail.com>
2023-08-02 00:04:26 +0500

    feat: add validation in input (2023-08-02, 0:04)

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <div class="tab-buttons mt-[30px]">
+    <div class="tab-buttons mt-[0px]">
       <button
         class="login w-[240px] h-[54px]"
         @click="setActive('tab-1')"
@@ -22,7 +22,7 @@
         <div class="for-example">
           <v-sheet
             width="480"
-            height="450"
+            height="480"
             class="mx-auto shadow-md px-[40px] rounded-md"
           >
             <div class="logo">
@@ -40,15 +40,17 @@
                 <label for="email" class="text-[14px] font-medium"
                   >E-Mail address</label
                 >
-
                 <input
                   id="email"
                   type="email"
                   class="block w-full mt-[5px] pl-3 pr-10 py-2 rounded-md border-black hover:shadow-lg"
                   placeholder="you@example.com"
+                  v-model="email"
                 />
+                <span v-if="!isEmailValid" class="text-red-600 text-sm mt-1"
+                  >Please enter a valid email address.</span
+                >
               </div>
-
               <div class="flex flex-col mt-[20px]">
                 <label for="password" class="text-[14px] font-medium"
                   >Password</label
@@ -59,6 +61,7 @@
                     :type="showPassword ? 'text' : 'password'"
                     class="block w-full mt-[5px] pl-3 pr-10 py-2 rounded-md border-black hover:shadow-lg"
                     placeholder="Your Password"
+                    v-model="password"
                   />
                   <div
                     class="absolute inset-y-0 right-0 pr-3 flex items-center"
@@ -94,7 +97,11 @@
                     </button>
                   </div>
                 </div>
+                <span v-if="!isPasswordValid" class="text-red-600 text-sm mt-1"
+                  >Password must be at least 8 characters long.</span
+                >
               </div>
+
               <p
                 class="text-sm text-gray-600 font-ьувшгь underline cursor-pointer ml-1 mt-4"
               >
@@ -102,7 +109,7 @@
               </p>
               <button
                 type="submit"
-                class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#e04b00] rounded-md hover:bg-gray- focus:outline-none focus:bg-gray-600 mt-[30px]"
+                class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#e04b00] rounded-md mt-[30px]"
               >
                 Login
               </button>
@@ -114,7 +121,7 @@
         <div class="for-example">
           <v-sheet
             width="480"
-            height="550"
+            height="620"
             class="mx-auto shadow-md px-[40px] rounded-md"
           >
             <div class="logo">
@@ -128,18 +135,21 @@
               <p class="font-bold text-[24px] text-center">
                 Create your sellcenter account!
               </p>
-              <div class="flex flex-col mt-[15px]">
-                <label for="email" class="text-[14px] font-medium"
-                  >E-Mail address</label
-                >
+							<div class="flex flex-col mt-[15px]">
+      <label for="email" class="text-[14px] font-medium">E-Mail address</label>
+      <input
+        id="email"
+        type="email"
+        class="block w-full mt-[5px] pl-3 pr-10 py-2 rounded-md border-black hover:shadow-lg"
+        placeholder="you@example.com"
+        v-model="email"
+        @input="validateEmail"
+      />
+      <span v-if="!isEmailValid" class="text-red-600 text-sm mt-1">
+        Please enter a valid email address.
+      </span>
+    </div>
 
-                <input
-                  id="email"
-                  type="email"
-                  class="block w-full mt-[5px] pl-3 pr-10 py-2 rounded-md border-black hover:shadow-lg"
-                  placeholder="you@example.com"
-                />
-              </div>
 
               <div class="flex flex-col mt-[20px]">
                 <label for="password" class="text-[14px] font-medium"
@@ -166,7 +176,6 @@
                         height="1em"
                         viewBox="0 0 640 512"
                       >
-                        <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                         <path
                           d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"
                         />
@@ -178,7 +187,6 @@
                         height="1em"
                         viewBox="0 0 576 512"
                       >
-                        <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                         <path
                           d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"
                         />
@@ -186,16 +194,22 @@
                     </button>
                   </div>
                 </div>
+								<span v-if="!isPasswordValid" class="text-red-600 text-sm mt-1"
+                  >Password must be at least 8 characters long.</span
+                >
               </div>
 
-              <p class="conditions text-[14px] mt-[20px]">
+              <p class="conditions text-[14px] mt-[20px] mb-[20px]">
                 The <span>General Terms and Conditions</span> apply. Information
                 on the way all data are processed can be found in our
                 <span>Privacy Policy</span>.
               </p>
               <label class="custom-checkbox">
-								
-                <input type="checkbox" v-model="isChecked"  @click="toggleShowCheckbox()"/>
+                <input
+                  type="checkbox"
+                  v-model="isChecked"
+                  @click="toggleShowCheckbox()"
+                />
                 <svg
                   class="icon"
                   xmlns="http://www.w3.org/2000/svg"
@@ -206,7 +220,7 @@
                   <!-- Insert your SVG arrow icon here -->
                   <path
                     v-if="isChecked"
-                    fill="#FFA500"
+                    fill="#FFFFFF"
                     d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
                   />
                 </svg>
@@ -215,21 +229,9 @@
                 and Kleinanzeigen (can be cancelled at any time in the account
                 settings).
               </label>
-
-              <!-- <div class="checkbox flex justify-between mt-[10px]">
-                <div class="div h-[13px] w-[25px]">
-                  <input type="checkbox" class="h-full w-full" />
-                </div>
-                <label class="text-sm">
-                  I would like to receive emails from mobile.de about offers,
-                  surveys and information on products and services from
-                  mobile.de and Kleinanzeigen (can be cancelled at any time in
-                  the account settings).
-                </label>
-              </div> -->
               <button
                 type="submit"
-                class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#e04b00] rounded-md hover:bg-gray- focus:outline-none focus:bg-gray-600 mt-[20px]"
+                class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#e04b00] rounded-md mt-[20px]"
               >
                 Register
               </button>
@@ -256,8 +258,12 @@ export default {
   },
   data() {
     return {
-      isChecked: true	,
+      isChecked: true,
       showPassword: false,
+      email: "",
+      password: "",
+      isEmailValid: true,
+      isPasswordValid: true,
     };
   },
   methods: {
@@ -265,7 +271,26 @@ export default {
       this.showPassword = !this.showPassword;
     },
     toggleShowCheckbox() {
-			this.isChecked = !this.isChecked;
+      this.isChecked = !this.isChecked;
+    },
+    validateEmail() {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      this.isEmailValid = emailRegex.test(this.email);
+    },
+
+    validatePassword() {
+      this.isPasswordValid = this.password.length >= 8;
+    },
+
+    signUp() {
+      // Validate email and password before submitting
+      this.validateEmail();
+      this.validatePassword();
+
+      if (this.isEmailValid && this.isPasswordValid) {
+        // Submit form or do other actions here
+        // ...
+      }
     },
   },
 };
@@ -300,10 +325,15 @@ export default {
   left: 0;
   width: 20px;
   height: 20px;
-  fill: #ffa500;
+  fill: #ffffff;
   border: 1px solid #000;
 }
 .custom-checkbox input[type="checkbox"]:checked + .icon {
-  fill: #ffa500;
+  fill: #ffffff;
+  background: #ffa500;
+}
+input:focus {
+	
+box-shadow: 0px 0px 1px 4px rgba(58, 190, 213, 0.671);
 }
 </style>
