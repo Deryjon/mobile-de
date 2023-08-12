68a87f02cd6afe7a802ae28bb07e585b368de888
Dery Jon <yusupoviskandar1005@gmail.com>
2023-08-12 14:42:26 +0500

    finish: finished payment tab-1 (2023-08-12, 14:42)

--- src/views/CarFilterPage/components/ConditionComponentBasic.vue
+++ src/views/CarFilterPage/components/ConditionComponentBasic.vue
@@ -1,188 +1,179 @@
 <template>
-<div class="condition p-[20px]">
-          <h3>Type and condition</h3>
-          <div class="radios-type flex gap-[244px] mt-[10px]">
-            <label for="condition-any" @click="selectCondition('Any')">
-              <input
-                type="radio"
-                v-model="selectedCondition"
-                :class="{
-                  'bg-transparent': selectedCondition !== 'Any',
-                  'bg-orange': selectedCondition === 'Any',
-                }"
-                class="ml-10px"
-              />
-              <span class="ml-[10px]">Any</span>
-            </label>
-            <label for="condition-any" >
-              <input
-                type="radio"
-                id="condition-any"
-                v-model="selectedCondition"
-								
-                :class="{
-                  'bg-transparent': selectedCondition !== 'New',
-                  'bg-orange': selectedCondition === 'New',
-                }"
-								@click="selectCondition('New')"
-              />
-              <span class="ml-[10px]">New</span>
-            </label>
-            <label for="condition-any" @click="selectCondition('Used')">
-              <input
-                type="radio"
-                id="condition-any"
-                v-model="selectedCondition"
-                :class="{
-                  'bg-transparent': selectedCondition !== 'Used',
-                  'bg-orange': selectedCondition === 'Used',
-                }"
-              />
-              <span class="ml-[10px]">Used</span>
-            </label>
-          </div>
-					<div class="conditions flex gap-[140px] mt-[20px]">
+  <div class="condition p-[20px]">
+    <h3>Type and condition</h3>
+    <div class="radios-type flex gap-[244px] mt-[10px]">
+      <label for="condition-any" @click="selectCondition('Any')">
+        <input
+          type="radio"
+          v-model="selectedCondition"
+          :class="{
+            'bg-transparent': selectedCondition !== 'Any',
+            'bg-orange': selectedCondition === 'Any',
+          }"
+          class="ml-10px"
+        />
+        <span class="ml-[10px]">Any</span>
+      </label>
+      <label for="condition-any">
+        <input
+          type="radio"
+          id="condition-any"
+          v-model="selectedCondition"
+          :class="{
+            'bg-transparent': selectedCondition !== 'New',
+            'bg-orange': selectedCondition === 'New',
+          }"
+          @click="selectCondition('New')"
+        />
+        <span class="ml-[10px]">New</span>
+      </label>
+      <label for="condition-any" @click="selectCondition('Used')">
+        <input
+          type="radio"
+          id="condition-any"
+          v-model="selectedCondition"
+          :class="{
+            'bg-transparent': selectedCondition !== 'Used',
+            'bg-orange': selectedCondition === 'Used',
+          }"
+        />
+        <span class="ml-[10px]">Used</span>
+      </label>
+    </div>
+    <div class="conditions flex gap-[140px] mt-[20px]">
+      <label
+        class="custom-checkbox flex items-center h-10 w-[155px] pb-[23px]"
+        :class="{ 'opacity-20': isRadioNewSelected }"
+      >
+        <input
+          type="checkbox"
+          :disabled="isRadioNewSelected"
+          class="form-checkbox h-5 w-5 text-indigo-600"
+          :class="{ 'bg-[#ccc] cursor-help': isRadioNewSelected }"
+          v-model="isCheckedRegister"
+          @click="toggleShowCheckbox(5)"
+        />
+        <svg
+          class="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+          width="1em"
+        >
+          <!-- Insert your SVG arrow icon here -->
+          <path
+            v-if="isCheckedRegister"
+            fill="#FFFFFF"
+            d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+          />
+        </svg>
 
-						<label
-						class="custom-checkbox flex items-center h-10 w-[155px] pb-[23px]"	:class="{ 'opacity-20': isRadioNewSelected }"
+        <span class="text-sm">Pre-Registration</span>
+      </label>
+      <label
+        class="custom-checkbox flex items-center h-10 w-[145px] pb-[23px]"
+        :class="{ 'opacity-20': isRadioNewSelected }"
+      >
+        <input
+          type="checkbox"
+          v-model="isCheckedEmploy"
+          @click="toggleShowCheckbox(0)"
+          :disabled="isRadioNewSelected"
+          class="form-checkbox h-5 w-5 text-indigo-600"
+        />
+        <svg
+          class="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+          width="1em"
+        >
+          <!-- Insert your SVG arrow icon here -->
+          <path
+            v-if="isCheckedEmploy"
+            fill="#FFFFFF"
+            d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+          />
+        </svg>
 
-						>
-							<input
-								type="checkbox"
-								:disabled="isRadioNewSelected"
-									class="form-checkbox h-5 w-5 text-indigo-600"
-									:class="{ 'bg-[#ccc] cursor-help': isRadioNewSelected }"
-								v-model="isCheckedRegister"
-								@click="toggleShowCheckbox(5)"
-							/>
-							<svg
-								class="icon"
-								xmlns="http://www.w3.org/2000/svg"
-								height="1em"
-								viewBox="0 0 448 512"
-								width="1em"
-							>
-								<!-- Insert your SVG arrow icon here -->
-								<path
-									v-if="isCheckedRegister"
-									fill="#FFFFFF"
-									d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-								/>
-							</svg>
-	
-							<span class="text-sm">Pre-Registration</span>
-						</label>
-						<label
-						class="custom-checkbox flex items-center h-10 w-[145px] pb-[23px]"	:class="{ 'opacity-20': isRadioNewSelected }"
+        <span class="text-sm">Employee's Car</span>
+      </label>
+      <label
+        class="custom-checkbox flex items-center h-10 w-[134px] pb-[23px]"
+        :class="{ 'opacity-20': isRadioNewSelected }"
+      >
+        <input
+          type="checkbox"
+          v-model="isCheckedClassic"
+          @click="toggleShowCheckbox(1)"
+          :disabled="isRadioNewSelected"
+          class="form-checkbox h-5 w-5 text-indigo-600"
+        />
+        <svg
+          class="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+          width="1em"
+        >
+          <!-- Insert your SVG arrow icon here -->
+          <path
+            v-if="isCheckedClassic"
+            fill="#FFFFFF"
+            d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+          />
+        </svg>
 
-						>
-							<input
-								type="checkbox"
-								v-model="isCheckedEmploy"
-								@click="toggleShowCheckbox(0)"
-								:disabled="isRadioNewSelected"
-									       
+        <span class="text-sm">Classic Vehicle</span>
+      </label>
+      <label
+        class="custom-checkbox flex items-center h-10 w-[190px] pb-[23px]"
+        :class="{ 'opacity-20': isRadioNewSelected }"
+      >
+        <input
+          type="checkbox"
+          v-model="isCheckedDemon"
+          @click="toggleShowCheckbox(2)"
+          :disabled="isRadioNewSelected"
+          class="form-checkbox h-5 w-5 text-indigo-600"
+        />
+        <svg
+          class="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+          width="1em"
+        >
+          <!-- Insert your SVG arrow icon here -->
+          <path
+            v-if="isCheckedDemon"
+            fill="#FFFFFF"
+            d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+          />
+        </svg>
 
-									class="form-checkbox h-5 w-5 text-indigo-600"
-							/>
-							<svg
-								class="icon"
-								xmlns="http://www.w3.org/2000/svg"
-								height="1em"
-								viewBox="0 0 448 512"
-								width="1em"
-							>
-								<!-- Insert your SVG arrow icon here -->
-								<path
-									v-if="isCheckedEmploy"
-									fill="#FFFFFF"
-									d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-								/>
-							</svg>
-	
-							<span class="text-sm">Employee's Car</span>
-						</label>
-						<label
-						class="custom-checkbox flex items-center h-10 w-[134px] pb-[23px]"	:class="{ 'opacity-20': isRadioNewSelected }"
-
-						>
-							<input
-								type="checkbox"
-								v-model="isCheckedClassic"
-								@click="toggleShowCheckbox(1)"
-								:disabled="isRadioNewSelected"
-									       
-
-									class="form-checkbox h-5 w-5 text-indigo-600"
-								
-							/>
-							<svg
-								class="icon"
-								xmlns="http://www.w3.org/2000/svg"
-								height="1em"
-								viewBox="0 0 448 512"
-								width="1em"
-							>
-								<!-- Insert your SVG arrow icon here -->
-								<path
-									v-if="isCheckedClassic"
-									fill="#FFFFFF"
-									d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-								/>
-							</svg>
-	
-							<span class="text-sm">Classic Vehicle</span>
-						</label>
-							<label
-								class="custom-checkbox flex  items-center h-10 w-[190px] pb-[23px]"	:class="{ 'opacity-20': isRadioNewSelected }"
-							>
-								<input
-									type="checkbox"
-									v-model="isCheckedDemon"
-									@click="toggleShowCheckbox(2)"
-									:disabled="isRadioNewSelected"
-									
-									class="form-checkbox h-5 w-5 text-indigo-600"
-								
-								/>
-								<svg
-									class="icon"
-									xmlns="http://www.w3.org/2000/svg"
-									height="1em"
-									viewBox="0 0 448 512"
-									width="1em"
-								>
-									<!-- Insert your SVG arrow icon here -->
-									<path
-										v-if="isCheckedDemon"
-										fill="#FFFFFF"
-										d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-									/>
-								</svg>
-		
-								<span class="text-sm">Demonstration Vehicle</span>
-							</label>
-					</div>
-        </div>
+        <span class="text-sm">Demonstration Vehicle</span>
+      </label>
+    </div>
+  </div>
 </template>
 <script>
-import {ref} from "vue"
-export default{
-	data(){
-return{
-	isAnySelected: false,
+import { ref } from "vue";
+export default {
+  data() {
+    return {
+      isAnySelected: false,
       isNewSelected: false,
       isUsedSelected: false,
       selectedCondition: "Any",
-			isRadioNewSelected: false,
-}
-	},
-	setup() {
-		const isCheckedRegister = ref(false);
+      isRadioNewSelected: false,
+    };
+  },
+  setup() {
+    const isCheckedRegister = ref(false);
     const isCheckedEmploy = ref(false);
     const isCheckedClassic = ref(false);
     const isCheckedDemon = ref(false);
-    
 
     const toggleShowCheckbox = (index) => {
       isCheckedRegister[index] = !isCheckedRegister[index];
@@ -195,9 +186,9 @@ return{
       isCheckedClassic,
       toggleShowCheckbox,
     };
-	},
-	methods:{
-		toggleAnySelection() {
+  },
+  methods: {
+    toggleAnySelection() {
       // Обработчик клика на "Any"
       if (this.isAnySelected) {
         // Если уже выбрано, меняем обратно на не выбрано
@@ -228,19 +219,19 @@ return{
       }
     },
     selectCondition(condition) {
-    this.selectedCondition = condition;
-    if (condition === 'New') {
-      this.isRadioNewSelected = true;
-			this.isCheckedRegister = false;
-      this.isCheckedEmploy = false;
-      this.isCheckedDemon = false;
-      this.isCheckedClassic = false;
-    } else {	
-      this.isRadioNewSelected = false;
-    }
+      this.selectedCondition = condition;
+      if (condition === "New") {
+        this.isRadioNewSelected = true;
+        this.isCheckedRegister = false;
+        this.isCheckedEmploy = false;
+        this.isCheckedDemon = false;
+        this.isCheckedClassic = false;
+      } else {
+        this.isRadioNewSelected = false;
+      }
+    },
   },
-	}
-}
+};
 </script>
 <style scoped>
 input[type="radio"] {
@@ -262,7 +253,8 @@ input[type="radio"] {
 }
 
 input[type="checkbox"]:disabled {
-	opacity: 0.5;  border: #ccc;
+  opacity: 0.5;
+  border: #ccc;
   /* Убираем возможность кликать на отключенный чекбокс */
   pointer-events: none;
   /* Убираем указатель курсора */
@@ -292,5 +284,4 @@ input[type="checkbox"]:disabled {
 .custom-checkbox input[type="checkbox"] {
   display: none;
 }
-
 </style>
