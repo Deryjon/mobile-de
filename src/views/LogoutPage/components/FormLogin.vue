780ef7ffd54c0d6638ce1c0a94a03f6aceb52f3f
Dery Jon <yusupoviskandar1005@gmail.com>
2023-09-07 01:36:44 +0500

    fix: change color in btn (2023-09-0, 1:36)

--- src/views/LogoutPage/components/FormLogin.vue
+++ src/views/LogoutPage/components/FormLogin.vue
@@ -251,7 +251,7 @@
                   the account settings).
                 </label>
                 <button
-                  @click="createNewUser(emailRegister, passwordRegister)"
+                  @click="createNewUser(emailRegister, passwordRegister, userCompany)"
                   type="submit"
                   :disabled="!isFormRegisterValid"
                   class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#e04b00] rounded-md mt-[20px]"
