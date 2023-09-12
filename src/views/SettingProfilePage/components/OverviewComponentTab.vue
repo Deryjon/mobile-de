baa303aba6031061ae6d8071234e637c363b67ae
allfred1 <87396510+allfred1@users.noreply.github.com>
2023-09-12 21:32:04 +0500

    Полностью переписан.

--- src/views/SettingProfilePage/components/OverviewComponentTab.vue
+++ src/views/SettingProfilePage/components/OverviewComponentTab.vue
@@ -1,178 +1,279 @@
-<template>
-    <div class="container-overview">
-            <h1 class="title"><span class="greetings-overview">Привет!</span>, что хотите сделать сегодня?</h1>
-            <div class="container-overview-cards">
-                <div v-for="card in cards">
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="title_card">{{card.title}}</div>
-                            <p class="card_description">{{card.text}}</p>
-                            <div class="icon-pos">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
-                                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                                </svg>
-                            </div>
-                        </div>
-                </div>
-            </div>
+<template>  
+
+<!-- INDEX -->
+
+<div class="container"> 
+  <div class="row-title">
+          <span>Привет!</span> Что желаете сделать сегодня?
+      </div> <!-- ./row-title -->
+  <div class="container-row">
+
+
+
+      <div class="element">
+        <div class="element-title">
+          Начать новый поиск сейчас
+        </div> <!-- /.element-title-->
+        <div class="element-desc">
+          <p>
+            1.445.836 Автомобилей
+          </p>
+        </div> <!-- /.element-desc -->
+
+        <div class="element-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+          </svg>
+        </div> <!-- /.element-icon -->
+      </div> <!-- /.element-->
+
+      <div class="container-ot">
+
+      </div> <!-- /.container-ot -->
+
+      <div class="element">
+        <div class="element-title">
+          Начать новый поиск сейчас
+        </div> <!-- /.element-title-->
+        <div class="element-desc">
+          <p>
+            1.445.836 Автомобилей
+          </p>
+        </div> <!-- /.element-desc -->
+
+        <div class="element-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+          </svg>
+        </div> <!-- /.element-icon -->
+      </div> <!-- /.element-->
+
+      <div class="container-ot"></div> <!-- /.container-ot -->
+
+      <div class="element">
+        <div class="element-title">
+          Начать новый поиск сейчас
+        </div> <!-- /.element-title-->
+        <div class="element-desc">
+          <p>
+            1.445.836 Автомобилей
+          </p>
+        </div> <!-- /.element-desc -->
+
+        <div class="element-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+          </svg>
+        </div> <!-- /.element-icon -->
+      </div> <!-- /.element-->
+
+      <div class="container-ot"></div> <!-- /.container-ot -->
+
+      <div class="element">
+        <div class="element-title">
+          Начать новый поиск сейчас
+        </div> <!-- /.element-title-->
+        <div class="element-desc">
+          <p>
+            1.445.836 Автомобилей
+          </p>
+        </div> <!-- /.element-desc -->
+
+        <div class="element-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+          </svg>
+        </div> <!-- /.element-icon -->
+      </div> <!-- /.element-->
+      <div class="container-ot"></div> <!-- /.container-ot -->
+
+<div class="element">
+  <div class="element-title">
+    Начать новый поиск сейчас
+  </div> <!-- /.element-title-->
+  <div class="element-desc">
+    <p>
+      1.445.836 Автомобилей
+    </p>
+  </div> <!-- /.element-desc -->
+
+  <div class="element-icon">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+    </svg>
+  </div> <!-- /.element-icon -->
+</div> <!-- /.element-->
+<div class="container-ot"></div> <!-- /.container-ot -->
+
+<div class="element">
+  <div class="element-title">
+    Начать новый поиск сейчас
+  </div> <!-- /.element-title-->
+  <div class="element-desc">
+    <p>
+      1.445.836 Автомобилей
+    </p>
+  </div> <!-- /.element-desc -->
+
+  <div class="element-icon">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+    </svg>
+  </div> <!-- /.element-icon -->
+</div> <!-- /.element-->
+  </div> <!-- /.conater-coloum -->
+
+  <!-- MY CARS -->
+  <div class="container-ot">
+    
+  </div> <!-- /.container-ot -->
+
+  <div class="mycars-title">
+    <h2>
+      Мои автомобили
+    </h2>
+  </div>
+
+
+    <div class="mycars">
+      <div class="mycars-status">
+        <svg class="iconButton" width="24" height="24" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 13.5h-8v-3h8v-8h3v8h8v3h-8v8h-3v-8z" fill="currentColor"></path></svg>
+        <div class="mycars-status-title">
+          <span>Пока нет доступного автомобиля</span>
         </div>
+        <div class="mycars-button-pos">
+            <button type="button" class="mycars-button">
+              <span class="mycars-button-title">
+                Добавить автомобиль
+              </span>
+            </button>
+          </div>
+      </div>
+    </div> 
 
-        <div class="dropgrid">
-            <div class="drop-header">
-                <!-- иконка, блять где иконки брать нахуй, чё за хуйня-->
-                <span> Мои машины </span>
-                <button class="drop-btn">Добавить автомобиль </button>
-            </div>
-                    <div class="drop-content">
-                        <div class="drop-item">
-                            <div class="drop-icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
-                                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                                </svg>
-                    </div>
-                    
-                    <span> Текст </span>
-                </div>
-            </div>
+</div> <!-- /.container -->
 
-        </div>
-
-
-
-
-
-        <div class="more-services-board"> <!-- v-for -->
-            <div class="msb-container">
-                <div class="msb-cards">
-                    <a href="#">
-                        <div class="msb-title-card"></div>
-                        <p class="msb-description-card"></p>
-                        <div class="msb-icon-display">
-                            <!-- Иконка -->
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
 
 <script>
-export default { // Language specific
-    data() {
-        return {
-            cards: [
-                {title: 'Карточка 1', text: 'Текст карточки 1'}, 
-                {title: 'Карточка 2', text: 'Текст карточки 2'},
-                {title: 'Карточка 3', text: 'Текст карточки 3'},
-                {title: 'Карточка 1', text: 'Текст карточки 1'}, 
-                {title: 'Карточка 2', text: 'Текст карточки 2'},
-                {title: 'Карточка 3', text: 'Текст карточки 3'}
-            ]  
-        }
-    }
+export default {
+  name: 'OverviewComponentTab',
+  // Your code here
 }
 </script>
 
-
-
 <style scoped>
-.title, .greetings-overview{
-    margin-left: 12px;
-    margin-top: 8px;
-    font-size: 32px;
-    line-height: 40px;
-    font-weight: 400;
-}
-.greetings-overview {
-    font-weight: bold;
-}
-.container-overview-cards{
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    overflow: hidden;
-    grid-gap: 16px;
-    padding: 16px;
-    margin-left: 12px;
-    grid-template-rows: minmax(0, 1fr); 
-    grid-auto-rows: minmax(0, 1fr); /* И эту строку */
 
-}   
-
-.card{
-    width: 250px;
-    box-sizing: border-box;
-    border-radius: 4px;
-    border: 4px solid aqua;
-    height: 200px;
-}
-.card-content{
-    padding: 16px;
+*{
+  box-sizing: border-box;
 }
 
-.title_card {
-    margin-bottom: 8px;
-    font-weight: bold; 
-}
-.card_description{
-    color: #666;
+.container-ot{
+  width: 18px;
 }
 
-.icon{ 
-    align-self: self-start;
-    width: 48px;
-    height: 48px;
+.container {
+  margin: 16px;
 }
 
-/* стили для карточки */
-
-.dropgrid {
-    display: flex;
-    justify-content: space-between;
-    position: relative;
+.container-coloum, .container-row {
+  display: flex;
+  flex-wrap: wrap;
 }
 
-.drop-header {
-    display: flex;
-    align-items: center;
+.container-coloum {
+  flex-direction: column;
 }
 
-.drop-btn {
-    margin-left: auto;
+.container-row {
+  flex-direction: row;
 }
 
-.drop-content {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    display: none;
+.row-title span{
+  font-weight: bold;
 }
 
-.drop-item {
-    display: flex;
-    align-items: center;
+.row-title, span, div {
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 40px;
+  margin-bottom: 8px;
+  margin-top: 0;
 }
 
-.drop-icon {
-    padding-right: 5px;
+.element{
+  border: 1px solid #757575;
+  border-radius: 4px;
+  width: 30%;
+  padding: 18px;
+  height: auto;
 }
 
-.dropgrid .drop-content{
-    display: block;
+.element-title{
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 28px;
+  margin-bottom: calc(18px/6);
+  padding-right: 0;
 }
 
-.drop-header{
-    border: 1px solid #757575;
-    width: 100%;
-    box-sizing: border-box;
+.element-desc, p {
+  color: #5a5e66;
+  font-size: 14px;
+  line-height: 20px;
 }
 
-.drop-content{
-    border-radius: 4px;
-    border: 1px solid #757575;
-    width: 100%;
-    box-sizing: border-box;
+.element-icon {
+  padding-top: 18px;
+  padding-bottom: 18px;
 }
+
+.element-icon, svg {
+  width: 48px; height:48px;
+}
+.mycars {
+  margin-top: 18px;
+  padding: 18px;
+  padding-right: 18px;
+  border: 1px solid #757575;
+}
+
+.mycars-title {
+  line-height: 32px;
+}
+
+.mycars-status {
+  align-items: center;
+  display: flex;
+  height: 24px;
+  justify-content: space-between;
+}
+
+.mycars-status, span {
+  color: #1e1f24;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 28px;
+  margin-right: calc(18px/6);
+}
+
+.mycars-button-pos{
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+}
+.mycars-button{
+  background-color: aqua;
+  padding: 8px;
+  align-content: center;
+  text-align: center;
+  border-radius: 14px;
+}
+.mycars-button-title{
+  padding: 28px;
+}
+
 </style>
+
 
 
