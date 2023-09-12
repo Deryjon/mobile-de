3fa235704895664819de543f2d2b7fa078f22a40
allfred1 <87396510+allfred1@users.noreply.github.com>
2023-09-12 16:55:47 +0500

    %date%

--- src/views/SettingProfilePage/components/OverviewComponentTab.vue
+++ src/views/SettingProfilePage/components/OverviewComponentTab.vue
@@ -16,22 +16,31 @@
                 </div>
             </div>
         </div>
-        <h1 class="title">Мои машины</h1>
-        <div class="headline">
-            <!-- Иконка -->
-            <div class="headline-title">
-                Не доступного автомобиля
+
+        <div class="dropgrid">
+            <div class="drop-header">
+                <!-- иконка, блять где иконки брать нахуй, чё за хуйня-->
+                <span> Мои машины </span>
+                <button class="drop-btn">Добавить автомобиль </button>
             </div>
-            <button type="button" class="addCardButton"> 
-                Добавить автомобиль
-            </button>
-            <div class="headline-tree-item">
-                <!-- Иконка -->
-                <spam> Никогда не забывайте о встрече: Мы напомним вам о следующий встрече для осмотра или замены шин.</spam>
-                <spam> Все данные об автомобиле: здесь вы всегда найдете все важные данные!.</spam>
-                <spam> Текущая рыночная стоимость стот ли продавать? Здесь вы мгновенно получите рыночную стоимость</spam>
+                    <div class="drop-content">
+                        <div class="drop-item">
+                            <div class="drop-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+                                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                                </svg>
+                    </div>
+                    
+                    <span> Текст </span>
+                </div>
             </div>
+
         </div>
+
+
+
+
+
         <div class="more-services-board"> <!-- v-for -->
             <div class="msb-container">
                 <div class="msb-cards">
@@ -117,7 +126,53 @@ export default { // Language specific
 
 /* стили для карточки */
 
+.dropgrid {
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+}
 
+.drop-header {
+    display: flex;
+    align-items: center;
+}
+
+.drop-btn {
+    margin-left: auto;
+}
+
+.drop-content {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    display: none;
+}
+
+.drop-item {
+    display: flex;
+    align-items: center;
+}
+
+.drop-icon {
+    padding-right: 5px;
+}
+
+.dropgrid .drop-content{
+    display: block;
+}
+
+.drop-header{
+    border: 1px solid #757575;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+.drop-content{
+    border-radius: 4px;
+    border: 1px solid #757575;
+    width: 100%;
+    box-sizing: border-box;
+}
 </style>
 
 
