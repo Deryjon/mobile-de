af0a6dd5c6282a0edc7fac96f5b6485a3cd00a88
allfred1 <87396510+allfred1@users.noreply.github.com>
2023-09-13 19:30:19 +0500

    Доработна

--- src/views/SettingProfilePage/components/OverviewComponentTab.vue
+++ src/views/SettingProfilePage/components/OverviewComponentTab.vue
@@ -122,126 +122,194 @@
   </div> <!-- /.element-icon -->
 </div> <!-- /.element-->
   </div> <!-- /.conater-coloum -->
-
   <!-- MY CARS -->
   <div class="container-ot">
     
   </div> <!-- /.container-ot -->
 
   <div class="mycars-title">
-    <h2>
+    <h2 class="mycars-title-txt">
       Мои автомобили
     </h2>
   </div>
+  
 
+  <!-- MY CARS MAYBE LIST? -->
+  <div class="contor"> 
 
-    <div class="mycars">
-      <div class="mycars-status">
-        <svg class="iconButton" width="24" height="24" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 13.5h-8v-3h8v-8h3v8h8v3h-8v8h-3v-8z" fill="currentColor"></path></svg>
-        <div class="mycars-status-title">
-          <span>Пока нет доступного автомобиля</span>
+  <div class="mycars-container">
+    <div class="mycars-status-title">
+      <svg class="mycars-title-icon" width="24" height="24" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 13.5h-8v-3h8v-8h3v8h8v3h-8v8h-3v-8z" fill="currentColor"></path></svg>
+      <span class="mycars-title-text"> 
+          Нет доступного автомобиля
+        </span>
+        <div class="mycars-status-addcar">
+          <button class="mycars-status-addccar-button">
+            <span class="mycars-status-addccar-button-text">
+              Добавить автомобиль
+            </span>
+          </button>
         </div>
-        <div class="mycars-button-pos">
-            <button type="button" class="mycars-button">
-              <span class="mycars-button-title">
-                Добавить автомобиль
-              </span>
-            </button>
-          </div>
       </div>
-    </div> 
-
-
-    <!-- MYY CARS -- INFO  -- TREE -->
-    <div class="mycars-container">
-      <div class="mycars-container-item">
-        <svg class="msssio-icon" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
-        <span class="msssio1-s"> Никогда больше не забывайте о встрече: мы напомним вам о следующей встрече для осмотра или замены шин.</span>
-      </div>
-      <div class="mycars-container-item">
-        <svg class="msssio-icon" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
-        <span class="msssio1-s"> Никогда больше не забывайте о встрече: мы напомним вам о следующей встрече для осмотра или замены шин.</span>
-      </div>
-      <div class="mycars-container-item">
-        <svg class="msssio-icon" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
-        <span class="msssio1-s"> Никогда больше не забывайте о встрече: мы напомним вам о следующей встрече для осмотра или замены шин.</span>
-      </div>
+        <!-- TREE(я не про себя если что...)-->
+    
     </div>
+    <div class="mycars-status-tree-title">
+        <span class="mycars-status-tree-title-txt">Цифровой двойник вашего автомобиля</span>
+      </div>
+      <div class="mycars-tree-container">
+        <div class="mycars-tree-info">
+          <svg class="mycars-tree-info-icon" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
+          <span class="mycars-tree-info-text"> Никогда больше не забывайте о встрече: мы напомним вам о следующий встрече для осмотра или замены шин</span>
+        </div>
+        <div class="mycars-tree-info">
+          <svg class="mycars-tree-info-icon" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
+          <span class="mycars-tree-info-text"> Все данные об автомобили: здесь вы всегда найдете все важные данные!</span>
+        </div>
+        <div class="mycars-tree-info">
+          <svg class="mycars-tree-info-icon" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
+          <span class="mycars-tree-info-text"> Текущая рыночная стоимость: стоит ли продавать? Здесь вы мгновенно получите рыночную стоимость</span>
+        </div>
+      </div>
+      </div>
 
-    <!-- <div class="mycars-status-info">
-          <div class="msssio">
-            Цифровой двойник вашего автомобиля
+      <!-- MORE SERVICES -->
+      <div class="mycars-title">
+        <h2 class="mycars-title-txt">
+          Больше услуг
+        </h2>
+      </div>
+
+      <div class="more-row">
+        <div class="more-element">
+          <div class="more-txt"> 
+            <div class="more-title">
+              Что я могу позволить себе?
+            </div>
+            <div class="more-desc">
+              <p>
+                Нечего. Молчать.
+              </p>
+            </div>
           </div>
-          <div class="mycars-status-info-tree">
-              <div class="msssio1">
-                <svg class="msssio-icon" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
-                <span class="msssio1-s"> Никогда больше не забывайте о встрече: мы напомним вам о следующей встрече для осмотра или замены шин.</span>
-              </div>
-
+          <div class="more-icon">
+            <svg class="more-icon-svg" width="20" height="20" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.899 6.667l1.768-1.768 7.1 7.101-7.1 7.101-1.768-1.768L14.232 12 8.899 6.667z" fill="currentColor"></path></svg>
           </div>
-        </div> -->
+        </div>
+        <div class="container-ot-msb"></div> <!-- /.container-ot -->
 
+        
+        <div class="more-element">
+          <div class="more-txt"> 
+            <div class="more-title">
+              Что я могу позволить себе?
+            </div>
+            <div class="more-desc">
+              <p>
+                Нечего. Молчать.
+              </p>
+            </div>
+          </div>
+          <div class="more-icon">
+            <svg class="more-icon-svg" width="20" height="20" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.899 6.667l1.768-1.768 7.1 7.101-7.1 7.101-1.768-1.768L14.232 12 8.899 6.667z" fill="currentColor"></path></svg>
+          </div>
+        </div>
+
+        <div class="container-ot-msb"></div> <!-- /.container-ot -->
+
+
+        <div class="more-element">
+          <div class="more-txt"> 
+            <div class="more-title">
+              Что я могу позволить себе?
+            </div>
+            <div class="more-desc">
+              <p>
+                Нечего. Молчать.
+              </p>
+            </div>
+          </div>
+          <div class="more-icon">
+            <svg class="more-icon-svg" width="20" height="20" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.899 6.667l1.768-1.768 7.1 7.101-7.1 7.101-1.768-1.768L14.232 12 8.899 6.667z" fill="currentColor"></path></svg>
+          </div>
+        </div>
+
+        <div class="container-ot-msb"></div> <!-- /.container-ot -->
+
+        <div class="more-element">
+          <div class="more-txt"> 
+            <div class="more-title">
+              Что я могу позволить себе?
+            </div>
+            <div class="more-desc">
+              <p>
+                Нечего. Молчать.
+              </p>
+            </div>
+          </div>
+          <div class="more-icon">
+            <svg class="more-icon-svg" width="20" height="20" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.899 6.667l1.768-1.768 7.1 7.101-7.1 7.101-1.768-1.768L14.232 12 8.899 6.667z" fill="currentColor"></path></svg>
+          </div>
+        </div>
+
+        <div class="container-ot-msb"></div> <!-- /.container-ot -->
+
+        
+      </div>
+      
 </div> <!-- /.container -->
 
 </template>
 
 <script>
 export default {
-  name: 'OverviewComponentTab',
-  // Your code here
+  // Компонент 
+  mounted() {
+    this.$nextTick(() => {
+      this.initAnimations();
+    })
+  },
+
+  methods: {
+    initAnimations() {
+      const elements = document.querySelectorAll('.element');
+      elements.forEach(el => {
+        el.classList.add('animate');  
+      });
+      
+      const moreElements = document.querySelectorAll('.more-element');  
+      moreElements.forEach(el => {
+        el.classList.add('animate'); 
+      });
+
+      // Получаем элемент с текстом
+      const textElement = document.querySelector('.span-row-ttile');
+
+      // Разбиваем текст на массив символов
+      const text = textElement.textContent.trim().split(''); 
+
+      text.forEach(char => {
+        const charElement = document.createElement('span'); 
+        charElement.textContent = char;
+        textElement.appendChild(charElement); 
+      });
+
+      textElement.querySelectorAll('span').forEach((char, index) => {
+        char.style.animationDelay = `${index * 50}ms`;
+      });
+    }
+  }
+
 }
+
 </script>
 
 <style scoped>
-
 *{
   box-sizing: border-box;
 }
 
-
-.mycars-container{
-  width: auto;
-  border: 1px solid #757575;
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-}
-
-.mycars-container-item{
-  display: flex;
-}
-
-.msssio1-s{
-  margin-left: 16px
-}
-
-/* .mycars-status-info-tree {
-  display: flex;
-}
-
-.msssio1 {
-  display: flex;
-  align-items: center;
-}
-
-.msssio{
-  margin-left: 24px;
-  font-size: 18px;
-  font-weight: bold;
-  line-height: 24px;
-  margin-bottom: calc(18px/2);
-}
-
-.msssio1-s{
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 24px;
-  margin-bottom: calc(18px/2);
-}
-
-.msssio-icon {
-  padding: 8px;
-} */
+ 
 
 /* INDEX */
 
@@ -312,52 +380,218 @@ export default {
 
 /* CARS   SECTION */
 
-.mycars {
-  display: flex;
-  align-items: center;
-  margin-top: 18px;
-  padding: 18px;
-  padding-right: 18px;
+.contor {
   border: 1px solid #757575;
+  padding:8px;
+  width: 95%;
 }
 
-.mycars-title {
-  line-height: 32px;
+
+.mycars-title-txt{
+  padding: 8px;
 }
 
-.mycars-status {
-  align-items: center;
+.mycars-container {
+  border: 1px solid #757575;
+  border-radius: 4px;
+  padding: 8px;
   display: flex;
-  height: 24px;
-  justify-content: space-between;
+  align-items: center;
+  padding-top: auto;
 }
 
-.mycars-status, span {
-  color: #1e1f24;
-  font-size: 20px;
-  font-weight: 500;
-  line-height: 28px;
-  margin-right: calc(18px/6);
-}
 
-.mycars-button-pos{
-  align-items: flex-end;
-  margin-left: auto;
+.mycars-title-icon {
   align-self: center;
 }
-.mycars-button{
-  background-color: aqua;
-  padding: 8px;
-  align-content: center;
-  text-align: center;
-  border-radius: 14px;
+.mycars-title-icon{
+  float: left;
+  margin-right: 10px;
+  width: 30px;
+  height: 30px;
 }
-.mycars-button-title{
-  padding: 28px;
+
+.mycars-status-title {
+  margin-right: 10px;
+  align-self: center;
+  font-size: 20px;
+  border-top: none;
+  font-weight: 500;
+  flex-grow: 1;
+}
+
+.mycars-status-tree-title-txt{
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: calc(18/2);
+}
+
+
+.mycars-title-icon, 
+.mycars-status-title,
+.mycars-status-addcar {
+  height: 30px;
+  line-height: 30px;
+}
+
+
+
+.mycars-status-addccar-button-text {
+  justify-content: center;
+  font-size: 14px;
+}
+.mycars-status-addcar{  
+  display: flex;
+  float: right;
+  align-content: space-between;
+  justify-content: flex-end;
+  margin-left: 18px;
+}
+
+
+/* CASRS TREE */
+
+
+.mycars-status-tree-title{
+  display: flex;
+  flex-direction: column;
+}
+
+.mycars-tree-container{
+  display: flex;
+  flex-direction: column;
+}
+
+.mycars-tree-info{
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 10px; 
+}
+
+.mycars-tree-info-icon,
+.mycars-tree-info-text {
+  height: 20px;
+  line-height: 20px;
+  max-width: 700px;
+} /* чтобы не выёбался */
+
+
+.mycars-tree-info-icon{
+  margin-right: 10px;
+}
+
+
+.mycars-status-tree-title-text{
+  font-size: 20px;
+  font-weight: 500;
+  padding-right: 18px;
+  padding-bottom: 8px ;
+}
+.mycars-tree-info-text{ 
+  font-size: 16px;
+  font-weight: 400;
+}
+
+/* MORE SERVICES */
+
+.container-ot-msb{
+  width: 32px;
+}
+
+.more-row{
+  margin-left: 14px;
+  margin-top: 14px;
+  padding-bottom: 14px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.more-element{
+  display: flex;
+  align-items: center;
+  width: 34% !important;
+  justify-content: space-between;
+  border: 1px solid #757575;
+  border-radius: 4px;
+}
+
+.more-txt{
+  padding: 8px;
+}
+
+.more-title {
+  flex: 1;
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: calc(18/2);
+}
+
+.more-desc{
+  font-size: 14px;
+  color: #5a5e66;
+}
+
+
+.more-icon-svg{
+  margin-left: 10px;
+}
+
+/* animation */ 
+@keyframes slideLeft {
+  from {
+    opacity: 0;
+    transform: translateX(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+
+@keyframes slideUp {
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.element {
+  animation: slideUp 0.5s ease-out;
+}
+
+.more-element {
+  animation: slideLeft 1s ease-out;
+}
+
+@keyframes slideUp {
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.span-row-ttile {
+  display: inline-block; 
+  animation: slideLeft 200ms ease-out;
 }
 
 
 </style>
+
+
+
+
 
 
 
