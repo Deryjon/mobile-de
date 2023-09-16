2aea29c3ff0d45c1b9a7a331671ab599a3b75661
allfred1 <87396510+allfred1@users.noreply.github.com>
2023-09-16 19:11:12 +0500

    idk

--- src/views/CarListPage/sections/YourAccount.vue
+++ src/views/CarListPage/sections/YourAccount.vue
@@ -3,8 +3,83 @@
     <div class="tabs w-[200px] h-[680px] bg-[#0000001f] p-[20px]">
     </div>
     <section
-      class="tabs-content settings relative w-[350px] sm:w-[550px] lg:w-[870px] xl:w-[870px] bg-[#0000001f] flex"
-    >
+      class="tabs-content settings relative w-[350px] sm:w-[550px] lg:w-[870px] xl:w-[870px] bg-[#0000001f] flex">
+
+
+    <div class="car-list">
+      <div class="car-element-list">
+        <div class="product">
+
+        <img src="https://img.classistatic.de/api/v1/mo-prod/images/15/15211295-0c43-40f8-bfe2-1643a9949c57?rule=mo-160.jpg" class="product-image">
+
+        <div class="product-info">
+
+        <div class="sponsored">СПОНСИРОВАНО</div>
+
+        <div class="product-title">Название товара</div>
+
+        <div class="product-date">abobaobaboa</div>
+
+        <div class="product-benefits">
+          <svg class="checksvg" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
+        Преимущество 1
+        </div>
+
+        <div class="product-benefits">
+          <svg class="checksvg" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
+        Преимущество 2
+        </div>
+
+        </div>
+
+        </div>
+
+
+        <div class="author-ii"> 
+
+          <div class="author-info">
+
+          <img src="https://images-ext-1.discordapp.net/external/XS-xx0gu5EkbyuLnrcUFRML-A-Iqagtzwwx9Ei38btQ/%3Fsize%3D512/https/cdn.discordapp.com/avatars/857551966166581288/a_52344222f92b823ac3c0de30376958af.gif" class="author-logo">
+
+          <div class="author-name">Имя автора</div>
+
+          <div class="author-rating">★★★★☆</div>
+
+          </div>
+
+          <div class="price-info">
+
+          <div class="price">1000 ₽</div>
+
+          <div class="vat">НДС не облагается</div>
+
+          </div>
+
+          <div class="contact">
+            <button> 
+              <span> Контакты </span>
+            </button>
+            <button> 
+              <span> Информация </span>
+            </button>
+          </div>
+        </div>
+
+
+
+    </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
 
     </section>
   </v-container>
@@ -43,7 +118,7 @@ export default {
       } else {
         this.isOpen = true;
       }
-      document.addEventListener(
+      document.addEventListener(  
         "click",
         this.closeCountryDropdownOnClickOutside
       );
@@ -101,4 +176,118 @@ export default {
 	background: #fff;
 	padding: 5px 8px;
 }
+
+
+
+/* CAR -- LIST  */
+
+
+.car-list {
+  display: flex;
+  flex-wrap: wrap
+}
+
+.car-element-list {
+  border: 1px solid #757575;
+  border-radius: 8px;
+  width: 550px;
+  height: 350px;
+}
+.product {
+  display: flex;
+  max-width: 33.33%;
+  align-items: center;
+}
+
+.product-image {
+  width: 200px;
+  height: auto; 
+}
+
+.product-info {
+  margin-left: 20px;
+}
+
+.sponsored {
+  border: 2px dashed #ccc;
+  padding: 5px;
+}
+
+.product-title {
+  font-weight: bold;
+  font-size: 1.2em;
+}
+
+.product-date {
+  color: #666;
+  font-size: 0.9em;  
+}
+
+.product-benefits {
+  display: flex;  
+  align-items: center;
+}
+
+.product-benefits img {
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+}
+
+.author-info {
+  display: flex;
+  align-items: center;
+}
+
+.author-logo {
+  border-radius: 4px;
+  width: 40px;
+  height: 40px;
+}
+
+.author-name {
+  margin-left: 10px;
+  font-size: 0.9em;
+}
+
+.author-rating {
+  margin-left: auto;
+  color: #f5c518;
+}
+
+.price-info {
+  text-align: right;
+}
+
+.author-ii {
+  padding-right: 12px;
+  margin-bottom: 4px;
+}
+.price {
+  font-weight: bold;
+  font-size: 1.3em; 
+}
+
+.vat {
+  font-size: 0.9em;
+  color: #666;
+}
+
+.contact {
+  padding-top: 4px;
+  text-align: right;
+  font-size: 0.8em;
+  padding-left: 8px;
+}
+
+.contact button {
+  padding: 4px;
+  border: 1px solid #757575;
+  border-radius: 6px;
+}
+
+.checksvg {
+  margin-right: 4px;
+}
+
 </style>
