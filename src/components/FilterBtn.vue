16bd499e1e34cb0556a24f0ac8bc3d9e0581e43f
Dery Jon <yusupoviskandar1005@gmail.com>
2023-09-18 03:01:55 +0500

    feat: add changes (2023-09-18, 3:01)

--- src/components/FilterBtn.vue
+++ src/components/FilterBtn.vue
@@ -6,7 +6,6 @@
 		@click="goCarList"
 		>
 
-    class="resultast_div flex items-center gap-[10px] lg:gap-[5px] w-[300px] lg:w-[160px] xl:w-[200px] h-[35px] bg-[#e04b00]  rounded-[8px] mt-[30px] lg:mt-[20px]">
 
     <svg
       class="icon m	l-[45px] sm:ml-[90px] lg:ml-[3px] xl:ml-[18px]"
