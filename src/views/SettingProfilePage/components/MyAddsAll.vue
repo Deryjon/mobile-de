c390e3ec39bf0255b1daa5276e01730ae3aa407b
Dery Jon <yusupoviskandar1005@gmail.com>
2023-09-20 17:33:01 +0500

    feat: add changes (2023-09-20, 17:32)

--- src/views/SettingProfilePage/components/MyAddsAll.vue
+++ src/views/SettingProfilePage/components/MyAddsAll.vue
@@ -3,7 +3,7 @@
     <div
       v-for="car in cars"
       :key="car.id"
-      class="card bor flex justify-between w-full h-[300px]  p-[20px]"
+      class="card bor bg-white flex justify-between w-full h-[300px]  p-[20px]"
     >
       <div class="img w-[400px] h-[260px] bor"></div>
       <div class="texts w-[520px]  h-[260px]">
@@ -57,14 +57,14 @@
 				<div class="flex gap-[10px] justify-end mt-[200px]">
         <button class=" flex items-center gap-[5px] bg-[#08829a] rounded-[4px] text-[14px] p-[8px] px-[20px] text-white" @click="cancelAdCar">
           <svg class="nQao3 hcDLf YgmFC" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 8l10 6 10-6v10a1 1 0 01-1 1H3a1 1 0 01-1-1V8zm0-3a1 1 0 011-1h18a1 1 0 011 1v1l-10 6L2 6V5z" fill="currentColor"></path></svg>
-					Contact
+					Delete
         </button>
         <button
           @click="addAdBasicCars"
           class="bg-transparent bor rounded-[4px] text-[14px] p-[8px] px-[20px] text-[#08829a] flex items-center gap-[5px]"
         >
 				<svg class="nQao3 hcDLf YgmFC" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M5 1h14a1 1 0 011 1v20.191a.5.5 0 01-.724.447L12 19l-7.276 3.638A.5.5 0 014 22.191V2a1 1 0 011-1zm4 4v10h2v-3h2c3 0 3-2 3-3.5S16 5 13 5H9zm2 2h2c1 0 1 1 1 1.5s0 1.5-1 1.5h-2V7z" fill="currentColor"></path></svg>
-          Park
+         Edit
         </button>
       </div>
 				
