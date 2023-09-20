3eb15b66c0b8bad3621ad7701a88e4170240e16f
Dery Jon <yusupoviskandar1005@gmail.com>
2023-09-20 17:22:29 +0500

    feat: add ads in car list (2023-09-20, 17:22)

--- src/views/CarListPage/sections/YourAccount.vue
+++ src/views/CarListPage/sections/YourAccount.vue
@@ -3,81 +3,81 @@
     <div class="tabs w-[200px] h-[680px] bg-[#0000001f] p-[20px]">
     </div>
     <section
-      class="tabs-content settings relative w-[350px] sm:w-[550px] lg:w-[870px] xl:w-[870px] bg-[#0000001f] flex p-[40px]">
+      class="tabs-content settings relative w-[350px] sm:w-[550px] lg:w-[870px] xl:w-[870px] bg-[#0000001f]  p-[40px]">
 
 
-    <div class="car-list w-[870px] h-[350px]">
-      <div class="car-element-list border-[#000] bor rounded-[8px] w-full p-[20px]">
-        <div class="product flex items-center max-w-[33.3%]">
-
-        <img src="https://img.classistatic.de/api/v1/mo-prod/images/15/15211295-0c43-40f8-bfe2-1643a9949c57?rule=mo-160.jpg" class="product-image">
-
-        <div class="product-info">
-        <div class="product-title">Название товара</div>
-
-        <div class="product-date">abobaobaboa</div>
-
-        <div class="product-benefits">
-          <svg class="checksvg" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
-        Преимущество 1
-        </div>
-
-        <div class="product-benefits">
-          <svg class="checksvg" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15.879l-5-5L1.879 13 9 20.121 23.121 6 21 3.879l-12 12z" fill="currentColor"></path></svg>
-        Преимущество 2
-        </div>
-
-        </div>
-
-        </div>
-
-
-        <div class="author-ii"> 
-
-          <div class="author-info">
-
-          <img src="https://images-ext-1.discordapp.net/external/XS-xx0gu5EkbyuLnrcUFRML-A-Iqagtzwwx9Ei38btQ/%3Fsize%3D512/https/cdn.discordapp.com/avatars/857551966166581288/a_52344222f92b823ac3c0de30376958af.gif" class="author-logo">
-
-          <div class="author-name">Имя автора</div>
-
-          <div class="author-rating">★★★★☆</div>
-
+			<div class="flex flex-wrap gap-[40px] justify-between mt-[20px]">
+    <div
+      v-for="car in cars"
+      :key="car.id"
+      class="card bor flex justify-between w-full h-[320px]  p-[20px]"
+    >
+      <div class="img w-[400px] h-[260px] bor"></div>
+      <div class="texts w-[520px]  h-[260px]">
+        <div class="name flex gap-[5px] text-[16px] font-semibold">
+          <div class="make">
+            {{ car.car_make }}
           </div>
-
-          <div class="price-info">
-
-          <div class="price">1000 ₽</div>
-
-          <div class="vat">НДС не облагается</div>
-
+          <div class="model">
+            {{ car.car_model }}
           </div>
-
-          <div class="contact">
-            <button> 
-              <span> Контакты </span>
-            </button>
-            <button> 
-              <span> Информация </span>
-            </button>
+          <div class="variant">
+            {{ car.car_variant }}
           </div>
         </div>
-
-
-
+        <div class="date-km flex gap-[5px]">
+          <div class="year">
+            {{ car.car_firt_date_year }}
+          </div>
+          •
+          <div class="mileage">{{ car.car_mileage }} km</div>
+          •
+          <div class="power">{{ car.car_power }} Hp</div>
+        </div>
+        <div class="car-body flex gap-[5px] text-[14px]">
+          <div class="car-body">
+            {{ car.car_body }}
+          </div>
+          •
+          <div class="fuel">
+            {{ car.car_fuel_type }}
+          </div>
+          •
+          <div class="transmission">
+            {{ car.car_transmission }}
+          </div>
+          •
+          <div class="hu">
+            HU
+            {{ car.car_hu_valid_until }}
+          </div>
+        </div>
+        <div class="car-body flex gap-[5px] text-[14px]">
+          <div class="car-body">
+            {{ car.car_number_door }}
+          </div>
+          Doors
+        </div>
+      </div>
+			<div class="price text-[18px] font-semibold">
+				<p class="price">€{{ car.car_price }}</p>
+				<div class="flex gap-[10px] justify-end mt-[200px]">
+        <button class=" flex items-center gap-[5px] bg-[#08829a] rounded-[4px] text-[14px] p-[8px] px-[20px] text-white" @click="cancelAdCar">
+          <svg class="nQao3 hcDLf YgmFC" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 8l10 6 10-6v10a1 1 0 01-1 1H3a1 1 0 01-1-1V8zm0-3a1 1 0 011-1h18a1 1 0 011 1v1l-10 6L2 6V5z" fill="currentColor"></path></svg>
+					Contact
+        </button>
+        <button
+          @click="addAdBasicCars"
+          class="bg-transparent bor rounded-[4px] text-[14px] p-[8px] px-[20px] text-[#08829a] flex items-center gap-[5px]"
+        >
+				<svg class="nQao3 hcDLf YgmFC" width="16" height="16" viewBox="0 0 24 24" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M5 1h14a1 1 0 011 1v20.191a.5.5 0 01-.724.447L12 19l-7.276 3.638A.5.5 0 014 22.191V2a1 1 0 011-1zm4 4v10h2v-3h2c3 0 3-2 3-3.5S16 5 13 5H9zm2 2h2c1 0 1 1 1 1.5s0 1.5-1 1.5h-2V7z" fill="currentColor"></path></svg>
+          Park
+        </button>
+      </div>
+				
+			</div>
     </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
+  </div>
     </section>
   </v-container>
 </template>
@@ -85,51 +85,26 @@
 import SettingsTab from "../components/SettingsComponentTab.vue";
 import OverviewTab from "../components/OverviewComponentTab.vue";
 import MyAdCarsTab from "../components/MyAdCarsTab.vue"
+import http from "../../../axios.config";
 export default {
   data() {
     return {
       userEmail: "",
+			userI: "",
       activeTab: "tab-2",
       isOpen: false,
+			cars: [],
     };
   },
   methods: {
-    setActive(tab) {
-      this.activeTab = tab;
+		fetchAds() {
+			this.userI = localStorage.getItem("u-i");
+      http.get(`/user/cars/${this.userI}?limit=100&offset=0`).then((res) => {
+        this.cars = res.data.data;
+        console.log(this.cars);
+      });
     },
-    isActive(tab) {
-      return this.activeTab === tab;
-    },
-
-    logOut() {
-      localStorage.removeItem("r-tok");
-      localStorage.removeItem("hasReloaded");
-      localStorage.removeItem("logged-in");
-      localStorage.setItem("logged-in", "false");
-      this.$router.push({ name: "home" });
-      window.location.reload();
-    },
-    openCountryDropdown() {
-      if (this.isOpen) {
-        this.isOpen = false;
-      } else {
-        this.isOpen = true;
-      }
-      document.addEventListener(  
-        "click",
-        this.closeCountryDropdownOnClickOutside
-      );
-    },
-    closeCountryDropdownOnClickOutside(event) {
-      const dropdownElement = this.$el.querySelector(".adds");
-      if (dropdownElement && !dropdownElement.contains(event.target)) {
-        this.isOpen = false;
-        document.removeEventListener(
-          "click",
-          this.closeCountryDropdownOnClickOutside
-        );
-      }
-    },
+    
   },
   mounted() {
     this.userEmail = localStorage.getItem("u-e");
@@ -138,7 +113,11 @@ export default {
     SettingsTab,
     OverviewTab,
 		MyAdCarsTab
-  },
+
+	},
+	created(){
+		this.fetchAds();
+	}
 };
 </script>
 <style scoped>
