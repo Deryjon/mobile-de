03f6e32e2e8336dbcc0f0d5c63c3ec30321b41cb
Dery Jon <yusupoviskandar1005@gmail.com>
2023-09-22 14:16:40 +0500

    feat: add changes (2023-09-22, 14:16)

--- src/views/SettingProfilePage/components/FormCreateAdd.vue
+++ src/views/SettingProfilePage/components/FormCreateAdd.vue
@@ -2080,147 +2080,95 @@
       <div class="filter-cars flex flex-wrap gap-x-[30px] mt-[20px]">
         <!-- cabrio -->
         <label
-          class="custom-checkbox custom-beige flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
+          class="custom-checkbox p-0 custom-beige flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
         >
           <input
-            type="checkbox"
-            v-model="isCheckedAlcantra"
-            @click="toggleShowCheckboxMaterial(0, 'Alcantra')"
+					type="radio"
+            @click="selectMaterial('Alcantra')"
+            v-model="selectedMaterial"
+            :class="{
+              'bg-transparent': selectedMaterial !== 'Alcantra',
+              'bg-orange': selectedMaterial === 'Alcantra',
+            }"
+            class="form-checkbox h-5 w-5 text-indigo-600"
           />
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-            width="1em"
-          >
-            <!-- Insert your SVG arrow icon here -->
-            <path
-              v-if="isCheckedAlcantra"
-              fill="#ffffff"
-              d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-            />
-          </svg>
           Alcantra
         </label>
         <label
-          class="custom-checkbox custom-brown flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
+          class="custom-checkbox p-0 custom-brown flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
         >
           <input
-            type="checkbox"
-            v-model="isCheckedCloth"
-            @click="toggleShowCheckboxMaterial(1, 'Cloth')"
+					type="radio"
+            @click="selectMaterial('Cloth')"
+            v-model="selectedMaterial"
+            :class="{
+              'bg-transparent': selectedMaterial !== 'Cloth',
+              'bg-orange': selectedMaterial === 'Cloth',
+            }"
+            class="form-checkbox h-5 w-5 text-indigo-600"
           />
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-            width="1em"
-          >
-            <!-- Insert your SVG arrow icon here -->
-            <path
-              v-if="isCheckedCloth"
-              fill="#ffffff"
-              d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-            />
-          </svg>
+          
           Cloth
         </label>
         <label
-          class="custom-checkbox custom-gold flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
+          class="custom-checkbox p-0 custom-gold flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
         >
           <input
-            type="checkbox"
-            v-model="isCheckedPartLeather"
-            @click="toggleShowCheckboxMaterial(2, 'Part leather')"
+					type="radio"
+            @click="selectMaterial('Part leather')"
+            v-model="selectedMaterial"
+            :class="{
+              'bg-transparent': selectedMaterial !== 'Part leather',
+              'bg-orange': selectedMaterial === 'Part leather',
+            }"
+            class="form-checkbox h-5 w-5 text-indigo-600"
           />
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-            width="1em"
-          >
-            <!-- Insert your SVG arrow icon here -->
-            <path
-              v-if="isCheckedPartLeather"
-              fill="#ffffff"
-              d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-            />
-          </svg>
+         
           Part leather
         </label>
         <label
-          class="custom-checkbox custom-green flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
+          class="custom-checkbox p-0 custom-green flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
         >
           <input
-            type="checkbox"
-            v-model="isCheckedVelour"
-            @click="toggleShowCheckboxMaterial(3, 'Velour')"
+					type="radio"
+            @click="selectMaterial('Velour')"
+            v-model="selectedMaterial"
+            :class="{
+              'bg-transparent': selectedMaterial !== 'Velour',
+              'bg-orange': selectedMaterial === 'Velour',
+            }"
+            class="form-checkbox h-5 w-5 text-indigo-600"
           />
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-            width="1em"
-          >
-            <!-- Insert your SVG arrow icon here -->
-            <path
-              v-if="isCheckedVelour"
-              fill="#ffffff"
-              d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-            />
-          </svg>
           Velour
         </label>
         <label
-          class="custom-checkbox custom-red flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
+          class="custom-checkbox p-0 custom-red flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
         >
           <input
-            type="checkbox"
-            v-model="isCheckedFullLeather"
-            @click="toggleShowCheckboxMaterial(4, 'Full Leather')"
+					type="radio"
+            @click="selectMaterial('Full leather')"
+            v-model="selectedMaterial"
+            :class="{
+              'bg-transparent': selectedMaterial !== 'Full leather',
+              'bg-orange': selectedMaterial === 'Full leather',
+            }"
+            class="form-checkbox h-5 w-5 text-indigo-600"
           />
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-            width="1em"
-          >
-            <!-- Insert your SVG arrow icon here -->
-            <path
-              v-if="isCheckedFullLeather"
-              fill="#ffffff"
-              d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-            />
-          </svg>
+         
           Full Leather
         </label>
         <label
-          class="custom-checkbox custom-red flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
+          class="custom-checkbox p-0 custom-red flex gap-[10px] text-[14px] items-center h-[40px] pb-[20px]"
         >
           <input
-            type="checkbox"
-            v-model="isCheckedOther"
-            @click="toggleShowCheckboxMaterial(5, 'Other')"
-          />
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-            width="1em"
-          >
-            <!-- Insert your SVG arrow icon here -->
-            <path
-              v-if="isCheckedOther"
-              fill="#ffffff"
-              d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-            />
-          </svg>
+					type="radio"
+            @click="selectMaterial('Other')"
+            v-model="selectedMaterial"
+            :class="{
+              'bg-transparent': selectedMaterial !== 'Other',
+              'bg-orange': selectedMaterial === 'Other',
+            }"
+            class="form-checkbox h-5 w-5 text-indigo-600" />
           Other
         </label>
       </div>
@@ -2914,9 +2862,9 @@ export default {
   },
   data() {
     return {
-			interiorAdd: false,
+			interiorAdd: true,
       fuelAdd: false,
-      basicAdd: true,
+      basicAdd: false,
       makes: [],
       models: [],
       selectedMark: "14600",
