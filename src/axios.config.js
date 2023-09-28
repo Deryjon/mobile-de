8c46b8a31938301b54d5234b8d64a0e86bd58abe
Dery Jon <yusupoviskandar1005@gmail.com>
2023-09-28 14:29:55 +0500

    feat: add engine section (2023-09-28, 14:29)

--- src/axios.config.js
+++ src/axios.config.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const http = axios.create()
 
-http.defaults.baseURL = 'https://api.behad.uz/api/v1'
+http.defaults.baseURL = 'https://https://sellcenter.onrender.com/'
 
 export default http
--- src/views/MotorbikeFilterPage/MotorbikeFilterView.vue
+++ src/views/MotorbikeFilterPage/MotorbikeFilterView.vue
@@ -1,11 +1,14 @@
 <template>
 <BasicDataSection/>
+<EngineSection/>
+
 </template>
 <script>
 import BasicDataSection from './sections/BasidDataSection/BasicDataSection.vue';
+import EngineSection from './sections/EngineSection/EngineSection.vue';
 
 export default{
-    components: { BasicDataSection }
+    components: { BasicDataSection, EngineSection }
 }
 </script>
 <style scoped>
