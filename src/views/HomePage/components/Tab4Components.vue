8ce35912304661405374b00cdb91a94c5a7acb08
Dery Jon <yusupoviskandar1005@gmail.com>
2023-09-29 16:29:25 +0500

    feat: add changes (2023-09-29, 16:28)

--- src/views/HomePage/components/Tab4Components.vue
+++ src/views/HomePage/components/Tab4Components.vue
@@ -1,114 +1,99 @@
 <template>
-  <div class="for-example ">
-    <div class="cars lg:flex  gap-[40px]">
-      <div class="cars_cars flex pl-[30px] lg:block gap-[20px] sm:gap-[100px]">
-        <div
-          class="cars_cars_car2  cursor-pointer lg:text-[14px] w-[150px] hover:bg-gray-200 "
-        >
-          <img
-            alt="Agricultural"
-            style="color: transparent"
-            src="../../../assets/images/truckess.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center w-full">Trucks over 7.5 t</h3>
-        </div>
-        <div
-          class="cars_cars_car  cursor-pointer lg:text-[14px] w-[150px]  hover:bg-gray-200"
-        >
-          <img
-            alt="buses"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/busess.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center">Buses and Coaches</h3>
-        </div>
+  <div class="for-example">
+    <div class="flex flex-wrap">
+      <div class="w-[200px] border-r border-b hover:bg-gray-300">
+        <img
+          alt="Agricultural"
+          style="color: transparent"
+          src="../../../assets/images/truckess.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px] ">Trucks over 7.5 t</h3>
       </div>
-      <div	 class="cars_cars_car flex lg:block gap-[20px] sm:gap-[100px]">
-        <div
-          class="cars_car2 w-[150px]  cursor-pointer lg:text-[14px] 10px] lg:mt-[0px] hover:bg-gray-200"
-        >
-          <img
-            alt="Agricultural"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/rucks-over-mini.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center lg:text-[14px]">Trucks over 7.5 t</h3>
-        </div>
-        <div
-          class="mt-[15px] w-[150px]  cursor-pointer lg:text-[14px] hover:bg-gray-200"
-        >
-          <img
-            alt="buses"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/buses.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center lg:text-[14px]">Buses and Coaches</h3>
-        </div>
+      <div class="w-[200px] border-r border-b hover:bg-gray-300">
+        <img
+          alt="buses"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/busess.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px]">Buses and Coaches</h3>
       </div>
-      <div class="cars_cars_car flex lg:block gap-[20px] sm:gap-[100px]">
-        <div class="cars_car2 w-[150px] hover:bg-gray-200 cursor-pointer	">
-          <img
-            alt="Agricultural"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/vans.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center lg:text-[14px]">Trucks over 7.5 t</h3>
-        </div>
-        <div class="cars_cars w-[150px] px-[10px] hover:bg-gray-200 cursor-pointer">
-          <img
-            alt="buses"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/machines.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center lg:text-[14px]">Buses and Coaches</h3>
-        </div>
+
+      <div class="w-[200px] border-r border-b hover:bg-gray-300 ">
+        <img
+          alt="Agricultural"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/rucks-over-mini.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px] ">Trucks over 7.5 t</h3>
       </div>
-      <div class="cars_cars w-[150px] flex lg:block gap-[20px] sm:gap-[100px]">
-        <div class="cars_cars_car2  cursor-pointer lg:text-[14px] hover:bg-gray-200">
-          <img
-            alt="Agricultural"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/coaches.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center">Trucks over 7.5 t</h3>
-        </div>
-        <div
-          class="cars_cars_car  cursor-pointer lg:text-[14px] flex_car hover:bg-slate-300"
-        >
-          <img
-            alt="buses"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/trucks-noim.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center">Buses and Coaches</h3>
-        </div>
+      <div class="w-[200px] border-r border-b hover:bg-gray-300">
+        <img
+          alt="buses"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/buses.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px] ">Buses and Coaches</h3>
       </div>
-      <div class="cars_cars flex lg:block">
-        <div class="cars_car2  cursor-pointer lg:text-[14px] hover:bg-gray-200">
-          <img
-            alt="Agricultural"
-            data-nimg="1"
-            style="color: transparent"
-            src="../../../assets/images/trucks-over.svg"
-            class="mx-auto w-[70px] lg:h-[50px]"
-          />
-          <h3 class="cars_taxt text-center">Trucks over 7.5 t</h3>
-        </div>
+
+      <div class="w-[200px] hover:bg-gray-300 border-r border-b ">
+        
+        <img
+          alt="Agricultural"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/vans.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px] ">Trucks over 7.5 t</h3>
+      </div>
+      <div class="w-[200px] hover:bg-gray-300 border-r ">
+        <img
+          alt="buses"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/machines.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px] ">Buses and Coaches</h3>
+      </div>
+
+      <div class="w-[200px] hover:bg-gray-300 border-r ">
+        <img
+          alt="Agricultural"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/coaches.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px]">Trucks over 7.5 t</h3>
+      </div>
+      <div class="w-[200px] hover:bg-gray-300 border-r ">
+        <img
+          alt="buses"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/trucks-noim.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px]">Buses and Coaches</h3>
+      </div>
+
+      <div class="w-[200px] hover:bg-gray-300 border-r ">
+        <img
+          alt="Agricultural"
+          data-nimg="1"
+          style="color: transparent"
+          src="../../../assets/images/trucks-over.svg"
+          class="mx-auto w-[70px] lg:h-[50px]"
+        />
+        <h3 class="text-center text-[14px]">Trucks over 7.5 t</h3>
       </div>
     </div>
   </div>
@@ -116,24 +101,4 @@
 <script>
 export default {};
 </script>
-<style>
-@media screen and (min-width: 1024px) {
-  .cars_cars_car2 {
-    border-right: 1px solid #35373d;
-    border-bottom: 1px solid #35373d;
-
-    outline: hidden;
-  }
-  .cars_car2 {
-    border-bottom: 1px solid #35373d;
-
-    outline: hidden;
-
-    text-align: center;
-  }
-  .cars_cars_car {
-    border-right: 1px solid #35373d;
-    text-align: center;
-  }
-}
-</style>
+<style scoped></style>
