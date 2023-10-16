dc034c58602b6f8908bb27d7f7608ba840f884a3
Muhammad Zayniddinov <muhammadzayniddinov0210@gmail.com>
2023-10-16 18:25:04 +0500

    feat: newspage edit

---
+++ src/views/FullNews/FullNews.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img :src="newsData.news_image_url" alt="" />
+  </div>
+</template>
+<script>
+import { useRoute } from "vue-router";
+export default {
+  data() {
+    return {
+      newsData: {},
+      id: useRoute().params.id,
+    };
+  },
+  methods: {
+    async fetchNews() {
+      await http
+        .get(`/news/list?limit=9&offset=0&lang=en/${this.id}`)
+        .then((res) => {
+          this.newsData = res.data.data;
+          console.log(this.newsData);
+        });
+    },
+  },
+  mounted() {
+    this.fetchNews();
+  },
+};
+</script>
+<style></style>
