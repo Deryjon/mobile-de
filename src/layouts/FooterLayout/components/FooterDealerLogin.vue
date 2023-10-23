9c4f0674a472d9e8cd37b810e61aa9fbcc5cc826
Dery Jon <yusupoviskandar1005@gmail.com>
2023-10-23 22:25:42 +0500

    feat: add changes (2023-10-23, 22:25)

--- src/layouts/FooterLayout/components/FooterDealerLogin.vue
+++ src/layouts/FooterLayout/components/FooterDealerLogin.vue
@@ -2,7 +2,7 @@
   <div class="socials">
 
     <h3 class="font-bold">Dealer</h3>
-    <ul class="flex flex-col gap-[10px]">
+    <ul class="flex flex-col ">
       <router-link class="text text-[14px]" :to="{ name: 'login' }"
         >Login</router-link
       >
@@ -17,15 +17,7 @@
     </ul>
   </div>
 
-		<h3 class="font-bold ">Dealer</h3>
-		<ul class=" flex flex-col">
-			<router-link class="text text-[14px]" :to="{ name: 'login' }">Login</router-link>
-			<router-link class="text text-[14px]" :to="{ name: 'login' }">Register</router-link>
-			<li class="text text-[14px]"><a href="">Our Offer</a></li>
-			<li class="text text-[14px]"><a href="">Price list</a></li>
-			<li class="text text-[14px]"><a href="">Terms and Conditions</a></li>
-		</ul>
-	</div>
+  
 
 </template>
 <script>
