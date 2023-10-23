1c29a6d5e35e46c32d4128f995819abfbaf98d6e
Dery Jon <yusupoviskandar1005@gmail.com>
2023-10-24 02:19:16 +0500

    feat: add changes (2023-24-10, 2:29)

--- src/layouts/FooterLayout/components/FooterDealerLogin.vue
+++ src/layouts/FooterLayout/components/FooterDealerLogin.vue
@@ -2,7 +2,7 @@
   <div class="socials">
 
     <h3 class="font-bold">Dealer</h3>
-    <ul class="flex flex-col ">
+    <ul class="flex flex-col">
       <router-link class="text text-[14px]" :to="{ name: 'login' }"
         >Login</router-link
       >
