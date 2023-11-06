b3a109974866b34ac83c8c44bba23fe286402e1f
Dery Jon <yusupoviskandar1005@gmail.com>
2023-11-07 01:05:42 +0500

    feat: add changes (2023-11-07, 1:05)

--- src/views/HomePage/sections/RentSection.vue
+++ src/views/HomePage/sections/RentSection.vue
@@ -3,21 +3,21 @@
     <v-container class="max-w-[1120px]">
       <div class="wrapper flex flex-col lg:flex-row lg:justify-between w-[1120px]">
         <div v-for="ad in ads" :key="ad.card_id" 
-        class="left-rent bg-[#f5f5f5] w-[350px] sm:w-[550px] lg:w-[420px] xl:w-[550px] flex gap-[15px] p-[30px]">
-          <div class="img lg:w-[200x] lg:h-[140px]">
-            <img :src="ad.card_image_url" alt="" class="w-full h-full object-cover" />
+        class="left-rent bg-[#f5f5f5]  w-[350px] sm:w-[550px] lg:w-[420px] xl:w-[550px] flex items-center gap-[15px] p-[30px]">
+          <div class="img lg:w-[200x] lg:h-[140px] ">
+            <img :src="ad.card_image_url" alt="" class="w-full h-full object-cover mt-[-35px]" />
           </div>
           <div class="text">
             <h3 class="title font-bold text-[18px]">
-              {{ $t("message.title.clicked") }}
+              {{ ad.card_title }}
             </h3>
             <p class="subtitle text-[15px] mt-[10px]">
-             {{ ad.card_text.length > 45
-              ? ad.card_text.substring(0, 45) + "..."
+             {{ ad.card_text.length > 70
+              ? ad.card_text.substring(0, 70) + "..."
               : ad.card_text}}
             </p>
             <button class="btn text-[15px] mt-[15px] border-gray-500" @click="redirectToLink(ad)">
-              {{ ad.card_title }}
+              More details
             </button>
           </div>
         </div>
