2eb6cbe642a811fefe5302b212699b8266eef01e
Dery Jon <133222583+Deryjon@users.noreply.github.com>
2023-12-21 10:09:41 +0500

    Update FooterCompanyLinks.vue

--- src/layouts/FooterLayout/components/FooterCompanyLinks.vue
+++ src/layouts/FooterLayout/components/FooterCompanyLinks.vue
@@ -34,7 +34,7 @@
 		  <h3 class="font-bold ">About Sellcenter</h3>
 		  <ul class=" flex flex-col ">
 		  <router-link class="text text-[10px] lg:text-[14px]" :to="{ name: 'about-us' }">Press</router-link>
-		  <router-link class="text text-[10px] lg:text-[14px]" :to="{ name: 'news' }">Work at Blocket</router-link>
+		  <router-link class="text text-[10px] lg:text-[14px]" :to="{ name: 'news' }">Work at Sellcenter</router-link>
 		  <router-link class="text text-[10px] lg:text-[14px]" :to="{ name: 'contact' }">Contact Us</router-link>
 		  <router-link class="text text-[10px] lg:text-[14px]" :to="{ name: 'home' }">Inspiration</router-link>
 			  <router-link class="text text-[10px] lg:text-[14px]" :to="{ name: 'home' }">Tips and guides</router-link>
