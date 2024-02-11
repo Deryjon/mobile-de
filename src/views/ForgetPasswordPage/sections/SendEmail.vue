4f3e90f991a5450ce9b9fefffa7c49fb804e2254
Dery Jon <yusupoviskandar1005@gmail.com>
2024-02-11 23:48:19 +0500

    feat: add changes

--- src/views/ForgetPasswordPage/sections/SendEmail.vue
+++ src/views/ForgetPasswordPage/sections/SendEmail.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="max-w-[1120px]">
-        <div v-if="!isNewPassword" class="box bg-[#0000001f] w-[300px]  mx-auto p-[20px] text-black">
+        <div v-if="!isNewPassword" class="box bg-[#0000001f] w-[320px]  mx-auto p-[20px] text-black">
             <p class="title text-black text-[14px] md:text-[16px] lg:text-[20px]">
                 {{ $t("message.register.forget") }}
             </p>
@@ -8,11 +8,18 @@
                 <img src="../../../assets/images/forgot-password.avif" alt="" class="w-full h-full object-cover">
             </div>
             <p>No problem. Just enter your email address and we’ll send you a link to create a new password. </p>
-            <div class="input-email mt-[30px]">
-                <p>Email Address</p>
-                <input v-model="emailSend" type="text" class="bor rounded-[5px] w-full px-[15px] py-[10px]">
+            <div class=" gap-[10px] items-center mt-[30px]" :class="{ 'flex': isTime }">
+
+                <div class="input-email ">
+                    <p>Email Address</p>
+                    <input v-model="emailSend" type="text" class="bor rounded-[5px] w-full px-[15px] py-[10px]">
+                </div>
+                <div class="pt-[20px]" v-if="isTime">
+                    <p>{{ minutes }}:{{ seconds < 10 ? '0' : '' }}{{ seconds }}</p>
+
+                </div>
             </div>
-            <button v-if="!isCode" class="btn bg-[#e04b00] px-auto w-full px-[15px] py-[10px] rounded-[5px] mt-[20px]"
+            <button v-if="!isCode" class="btn text-white bg-[#e04b00] px-auto w-full px-[15px] py-[10px] rounded-[5px] mt-[20px]"
                 @click="sendEmail">Send password
                 link</button>
 
@@ -28,7 +35,7 @@
             </div>
         </div>
         <div v-else
-            class="box bg-[#05030380] w-[300px] sm:w-[450px] md:w-[550px] lg:w-[700px]  mx-auto p-[20px] text-white">
+            class="box bg-[#0000001f] w-[300px] sm:w-[450px] md:w-[550px] lg:w-[700px]  mx-auto p-[20px] text-white">
             <p class="title text-white text-[14px] md:text-[16px] lg:text-[20px]">
                 New password
             </p>
@@ -60,6 +67,10 @@ export default {
             newPassword: "",
             isCode: false,
             isNewPassword: false,
+            isTime: false,
+            minutes: 2,
+            seconds: 0,
+            timer: null
         }
     },
     methods: {
@@ -68,6 +79,9 @@ export default {
                 email: this.emailSend
             }).then((res) => {
                 this.isCode = !this.isCode
+                this.isTime = !this.isTime
+                this.timer = setInterval(this.countDown, 1000);
+
             })
         },
         sendCode() {
@@ -84,13 +98,26 @@ export default {
                 email: this.emailSend,
                 new_password: this.newPassword
             }).then((res) => {
-                this.$router.push({ name: "home" })
+                this.$router.push({ name: "login" })
                 console.log(res.data);
             })
+        },
+        countDown() {
+            if (this.seconds === 0) {
+                if (this.minutes === 0) {
+                    clearInterval(this.timer);
+                    return;
+                }
+                this.minutes--;
+                this.seconds = 59;
+            } else {
+                this.seconds--;
+            }
         }
 
 
-    }
+    },
+
 }
 </script>
 <style scoped>
